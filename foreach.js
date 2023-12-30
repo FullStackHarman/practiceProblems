a047cebaad7100a8a19436263e74472824548fa1
Harman Preet Singh <harmanprt5142@gmail.com>
2023-12-30 22:50:54 +0530

    final commit

---
+++ foreach.js
@@ -0,0 +1,236 @@
+
+
+/*---------------------------------------forEach Easy practice problems--------------------------------------- */
+// Write a function calculateSum that takes an array of numbers as input and returns the sum of all elements using the forEach loop.
+
+
+function calculateSum(arr) {
+    // Your code here
+}
+
+// Example Usage
+const nums = [1, 2, 3, 4, 5];
+console.log(calculateSum(nums)); // Output should be 15
+// Problem 02: Counting Odd Numbers
+
+// Problem Statement:
+// Write a function countOddNumbers that takes an array of integers as input and returns the count of odd numbers using the forEach loop.
+
+
+function countOddNumbers(arr) {
+    // Your code here
+}
+
+// Example Usage
+const integers = [1, 2, 3, 4, 5];
+console.log(countOddNumbers(integers)); // Output should be 3
+// Problem 03: Finding Maximum Element
+
+// Problem Statement:
+// Write a function findMaxElement that takes an array of numbers as input and returns the maximum element using the forEach loop.
+
+
+function findMaxElement(arr) {
+    // Your code here
+}
+
+// Example Usage
+const nams = [10, 5, 8, 20, 3];
+console.log(findMaxElement(nams)); // Output should be 20
+// Problem 04: Concatenate Strings
+
+// Problem Statement:
+// Write a function concatenateStrings that takes an array of strings as input and concatenates them into a single string using the forEach loop.
+
+
+function concatenateStrings(arr) {
+    // Your code here
+}
+
+// Example Usage
+const str = ["Hello", " ", "World", "!"];
+console.log(concatenateStrings(str)); // Output should be "Hello World!"
+// Problem 05: Doubling Each Element
+
+// Problem Statement:
+// Write a function doubleElements that takes an array of numbers as input and returns a new array where each element is doubled, using the forEach loop.
+
+
+function doubleElements(arr) {
+    // Your code here
+}
+
+// Example Usage
+const numbs = [1, 2, 3, 4, 5];
+console.log(doubleElements(numbs)); // Output should be [2, 4, 6, 8, 10]
+
+
+/*---------------------------------------forEach Medium practice problems--------------------------------------- */
+
+// Problem 01: Filtering Prime Numbers
+
+// Problem Statement:
+// Write a function filterPrimes that takes an array of integers as input and returns a new array containing only the prime numbers using the forEach loop.
+
+
+function filterPrimes(arr) {
+    // Your code here
+}
+
+// Example Usage
+const namste = [2, 3, 5, 6, 8, 11, 13];
+console.log(filterPrimes(namste)); // Output should be [2, 3, 5, 11, 13]
+// Problem 02: Mapping Squares of Odd Numbers
+
+// Problem Statement:
+// Write a function mapSquaresOfOdd that takes an array of integers as input and returns a new array where the squares of odd numbers are calculated using the forEach loop.
+
+
+function mapSquaresOfOdd(arr) {
+    // Your code here
+}
+
+// Example Usage
+const numbers = [1, 2, 3, 4, 5];
+console.log(mapSquaresOfOdd(numbers)); // Output should be [1, 2, 9, 4, 25]
+
+// Problem 03: Sum of Positive Numbers
+
+// Problem Statement:
+// Write a function sumOfPositive that takes an array of numbers as input and returns the sum of all positive elements using the forEach loop.
+
+
+function sumOfPositive(arr) {
+    // Your code here
+}
+
+// Example Usage
+const sNumbers = [-2, 5, -8, 10, -3];
+console.log(sumOfPositive(Snumbers)); // Output should be 15
+
+// Problem 04: Reversing Array Elements
+
+// Problem Statement:
+// Write a function reverseArray that takes an array as input and returns a new array with the elements reversed using the forEach loop.
+
+
+function reverseArray(arr) {
+    // Your code here
+}
+
+// Example Usage
+const array = [1, 2, 3, 4, 5];
+console.log(reverseArray(array)); // Output should be [5, 4, 3, 2, 1]
+// Problem 05: Grouping Elements by Length
+
+// Problem Statement:
+// Write a function groupByLength that takes an array of strings as input and returns an object where keys are the lengths of strings and values are arrays of strings with corresponding lengths, using the forEach loop.
+
+
+function groupByLength(arr) {
+    // Your code here
+}
+
+// Example Usage
+const strings = ["apple", "banana", "pear", "grape"];
+console.log(groupByLength(strings));
+// Output should be { 5: ['apple', 'grape'], 6: ['banana'], 4: ['pear'] }
+
+
+
+
+
+
+
+
+
+/*---------------------------------------forEach Hard practice problems--------------------------------------- */
+// Problem 01: Matrix Transposition
+
+// Problem Statement:
+// Write a function transposeMatrix that takes a 2D array (matrix) as input and returns a new matrix representing the transpose of the original matrix using the forEach loop.
+
+
+function transposeMatrix(matrix) {
+    // Your code here
+}
+
+// Example Usage
+const inputMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(transposeMatrix(inputMatrix));
+// Output should be:
+// [
+//    [1, 4, 7],
+//    [2, 5, 8],
+//    [3, 6, 9]
+// ]
+// Problem 02: Sum of Diagonal Elements
+
+// Problem Statement:
+// Write a function sumDiagonalElements that takes a square matrix as input and returns the sum of diagonal elements (both primary and secondary diagonals) using the forEach loop.
+
+
+function sumDiagonalElements(matrix) {
+    // Your code here
+}
+
+// Example Usage
+const squareMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(sumDiagonalElements(squareMatrix)); // Output should be 15
+// Problem 03: Prime Numbers in Subarrays
+
+// Problem Statement:
+// Write a function findPrimesInSubarrays that takes an array of arrays of integers as input and returns an array of boolean values indicating whether each subarray contains at least one prime number, using the forEach loop.
+
+
+function findPrimesInSubarrays(arrays) {
+    // Your code here
+}
+
+// Example Usage
+const arrayOfArrays = [
+    [2, 4, 6],
+    [3, 9, 12],
+    [8, 10, 15]
+];
+console.log(findPrimesInSubarrays(arrayOfArrays)); // Output should be [true, true, false]
+// Problem 04: Unique Characters in Strings
+
+// Problem Statement:
+// Write a function hasUniqueCharacters that takes an array of strings as input and returns an array of boolean values indicating whether each string has all unique characters, using the forEach loop.
+
+
+function hasUniqueCharacters(strings) {
+    // Your code here
+}
+
+// Example Usage
+const stringArray = ["abc", "def", "aba"];
+console.log(hasUniqueCharacters(stringArray)); // Output should be [true, true, false]
+// Problem 05: Fibonacci Sequence Generator
+
+// Problem Statement:
+// Write a function generateFibonacci that takes an integer n as input and returns an array containing the first n elements of the Fibonacci sequence using the forEach loop.
+
+
+
+function generateFibonacci(n) {
+    // Your code here
+}
+
+// Example Usage
+const n = 5;
+console.log(generateFibonacci(n)); // Output should be [0, 1, 1, 2, 3]
+
+
+
+
+
