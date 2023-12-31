7b93067a895fd357d6aa7899823f8f64570b8f30
Harman Preet Singh <harmanprt5142@gmail.com>
2023-12-31 20:33:20 +0530

    final commit

---
+++ splice.js
@@ -0,0 +1,126 @@
+
+// Problem 01:
+// State the Problem: Write a function that uses the splice method to insert the elements 'a', 'b', and 'c' into an array at index 2.
+
+
+function insertElements(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array1 = [1, 2, 3, 4, 5];
+insertElements(array1);
+// The modified array should be [1, 2, 'a', 'b', 'c', 3, 4, 5]
+
+
+// Problem 02:
+// State the Problem: Implement a function that uses the splice method to remove the elements at index 2 and 3 from an array.
+
+function removeElements(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+removeElements(array2);
+// The modified array should be ['a', 'b', 'e']
+
+
+// Problem 03:
+// State the Problem: Create a function that uses the splice method to replace the elements at index 1 and 2 with the elements 'x' and 'y'.
+
+function replaceElements(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array3 = ['a', 'b', 'c', 'd', 'e'];
+replaceElements(array3);
+// The modified array should be ['a', 'x', 'y', 'd', 'e']
+//Problem 04:
+// Create a function that use splice method to remov d and e in the given array and replace it wtih x,y,z
+
+ function removeAndInsert(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array4 = ['a', 'b', 'c', 'd', 'e'];
+removeAndInsert(array4);
+// The modified array should be ['a', 'b', 'c', 'x', 'y', 'z']
+
+
+// Problem 05:
+// State the Problem: Implement a function that utilizes the splice method to remove the first element and insert 'x' and 'y' at the beginning of the array.
+
+function removeAndInsertAtStart(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array5 = ['a', 'b', 'c', 'd', 'e'];
+removeAndInsertAtStart(array5);
+// The modified array should be ['x', 'y', 'b', 'c', 'd', 'e']
+// Problem 06:
+// State the Problem: Create a function that uses splice to remove the elements at index 2 to 4 (inclusive) from an array.
+
+function removeRange(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array6 = ['a', 'b', 'c', 'd', 'e'];
+removeRange(array6);
+// The modified array should be ['a', 'b', 'e']
+
+
+
+// Problem 07:
+// State the Problem: Write a function that uses splice to insert the elements 'x', 'y', and 'z' at index 0 without removing any elements.
+
+function insertAtBeginning(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array7 = ['a', 'b', 'c', 'd', 'e'];
+insertAtBeginning(array7);
+// The modified array should be ['x', 'y', 'z', 'a', 'b', 'c', 'd', 'e']
+
+
+// Problem 08:
+// State the Problem: Implement a function that uses splice to reverse the order of elements in an array.
+
+
+function reverseArray(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array8 = ['a', 'b', 'c', 'd', 'e'];
+reverseArray(array8);
+// The modified array should be ['e', 'd', 'c', 'b', 'a']
+
+// Problem 09:
+// State the Problem: Create a function that uses splice to remove the last element from an array and then insert 'x', 'y', and 'z' at its position.
+
+function removeAndInsertAtEnd(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array9 = ['a', 'b', 'c', 'd', 'e'];
+removeAndInsertAtEnd(array9);
+// The modified array should be ['a', 'b', 'c', 'd', 'x', 'y', 'z']
+
+// Problem 10:
+// State the Problem: Write a function that uses splice to remove all elements from an array starting from index 2 to the end.
+
+function removeAllFromIndex(arr) {
+  // Your code here
+}
+
+// Example usage:
+const array = ['a', 'b', 'c', 'd', 'e'];
+removeAllFromIndex(array);
+// The modified array should be ['a', 'b']
